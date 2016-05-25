b80f4832af4b77f043b0b69b3c5d6dd7bff0d6b2
Spencer Christopherson <schristopherson@gmail.com>
2016-05-25 13:30:00 -0600

    recent activity working

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,11 +8,8 @@ class HomeController < ApplicationController
   	# mordue 221
   	# mardesich-smith 10
   	
-
 		agent = Mechanize.new
 		page = agent.get('http://www.myresaleweb.com/')
-
-		# store_form = page.forms[1]
 
 		# store_form['consignorid'] = '131'
 		# store_form['consignorlastname'] = 'awesome'
@@ -21,84 +18,56 @@ class HomeController < ApplicationController
 		@recent_activity_link
 		@recent_activity_page
 		@recent_activity_raw
+		@view_items_link
+		@view_items_page
+		@view_items_raw
 
 		@recent_activity = []
-		# @recent_activity_date = []
-		# @recent_activity_description = []
-		# @recent_activity_price = []
-
-
-		def parse_date(str)
-			
-			date = /.+([0-9]{2}\/[0-9]{2}\/[0-9]{4}).+/ =~ str && $1
-
-			if date == nil
-				date = /.+([0-9]{1}\/[0-9]{2}\/[0-9]{4}).+/ =~ str && $1
-			end
-			if date == nil
-				date = /.+([0-9]{2}\/[0-9]{1}\/[0-9]{4}).+/ =~ str && $1
-			end
-			if date == nil
-				date = /.+([0-9]{1}\/[0-9]{1}\/[0-9]{4}).+/ =~ str && $1
-			end
-			return date	
-		end
-
-		def parse_description(str)
-			
-			str = str[17...-6]
-			return str
-		end
-
-		def parse_price(str)
-			str = str[18...-6]
-			return str
-		end
+		@view_items = []
 
 		def recent_activity(raw)
 			
 			date = []
 			description = []
 			price = []
-
 			des_index = 4
 			price_idex = 5
 
 			raw.each_with_index do |r, index|
 				if index % 3 == 0 && index != 0
 					str = r.to_s
-					date.push(parse_date(str))
-					# @recent_activity_date.push(date)
+					date.push(str)
 				end
 				
 				if index == des_index
 					str = r.to_s
-					description.push(parse_description(str))
-					# @recent_activity_description.push(description)
+					description.push(str)
 					des_index += 3
 				end
 
 				if index == price_idex
 					str = r.to_s
-					price.push(parse_price(str))
-					# @recent_activity_price.push(price)
+					price.push(str)
 					price_idex +=3
-					# binding.pry
 				end
-
-				
-				# binding.pry
 			end
 			
 			date.each_with_index do |d, index|
-				
-				@recent_activity.push(Recent_activity.new(date[index], description[index], price[index])) 
 
+				recent = Recent_activity.new(date[index], description[index], price[index])
+				recent.parse_strings
+				@recent_activity.push(recent) 
 			end
-			 binding.pry
-
 		end
 
+		def view_items(raw)
+
+			item_num = [], description = [], date_in = [], date_out = []
+			price = [], status = [], count = []
+
+
+			 binding.pry
+		end
 
 		def populate_forms
 
@@ -108,7 +77,6 @@ class HomeController < ApplicationController
 			@store_form['consignorlastname'] = current_user.my_last_name
 
 			checkforms
-		
 		end
 
 		def checkforms 
@@ -119,44 +87,23 @@ class HomeController < ApplicationController
 				@page = @store_form.submit
 				@recent_activity_link = @page.link_with(text: 'Recent Activity')
 				@recent_activity_page = @recent_activity_link.click
-
 				@recent_activity_raw = @recent_activity_page.search("td.main")
-				# items.each 
-				# str = @recent_activity[3].to_s
-				# str = /.+([0-9]{1}\/[0-9]{2}\/[0-9]{4}).+/ =~ str && $1
-				 # date = parse_date(str)
-
 				recent_activity(@recent_activity_raw)
+
+				@view_items_link = @page.link_with(text: 'View Items')
+				@view_items_page = @view_items_link.click
+				@view_items_raw = @view_items_page.search("td.main")
 				
-
 				# binding.pry
+				view_items(@view_items_raw)
+
+
+
+
 			end
-
-
-
-			
-
-
 		end
-
-
-		
-		
-		
-		
-		
-		
-
-		# page = store_form.submit
-
-		# recent_activity_link = page.link_with(text: 'Recent Activity')
-		# recent_activity_page = recent_activity_link.click
-
 
 	populate_forms
 
-
-		page
-  	# binding.pry
   end
 end
--- app/controllers/recent_activity.rb
+++ app/controllers/recent_activity.rb
@@ -9,21 +9,21 @@ class Recent_activity
 
 	def parse_strings()
 
-		@date = @date[18...-6]
-		@price = @price[17...-6]
+		@price = @price[18...-6]
+		@description = @description[17...-6]
 		
-		@date_test = /.+([0-9]{2}\/[0-9]{2}\/[0-9]{4}).+/ =~ @date && $1
+		date_test = /.+([0-9]{2}\/[0-9]{2}\/[0-9]{4}).+/ =~ @date && $1
 
-		if @date_test == nil
-			@date_test = /.+([0-9]{1}\/[0-9]{2}\/[0-9]{4}).+/ =~ @date && $1
+		if date_test == nil
+			date_test = /.+([0-9]{1}\/[0-9]{2}\/[0-9]{4}).+/ =~ @date && $1
 		end
-		if @date_test == nil
-			@date_test = /.+([0-9]{2}\/[0-9]{1}\/[0-9]{4}).+/ =~ @date && $1
+		if date_test == nil
+			date_test = /.+([0-9]{2}\/[0-9]{1}\/[0-9]{4}).+/ =~ @date && $1
 		end
-		if @date_test == nil
-			@date_test = /.+([0-9]{1}\/[0-9]{1}\/[0-9]{4}).+/ =~ s@date && $1
+		if date_test == nil
+			date_test = /.+([0-9]{1}\/[0-9]{1}\/[0-9]{4}).+/ =~ @date && $1
 		end
-		@date = @date_test
+		@date = date_test
 		
 
 	end
