c9f026a6f371b6264d9d376471b75e31b04f5f1f
Spencer Christopherson <schristopherson@gmail.com>
2016-06-02 12:11:06 -0600

    configuring devise production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'https://iconoclad.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'iconoclad.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
     :address    => "smtp.sendgrid.net",
     :port       => 25,
